9ffa660294ae847ff3f5ed985f3872d55a20921f
Alisson Rego <alissondroid07@gmail.com>
2023-10-22 18:38:25 -0300

    Organização de pastas.

---
+++ src/main/java/edu/alisson/imgupload/entity/ImageData.java
@@ -0,0 +1,23 @@
+package edu.alisson.imgupload.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class ImageData {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String fileName;
+    private String fileType;
+    @Lob
+    private byte[] data;
+}
