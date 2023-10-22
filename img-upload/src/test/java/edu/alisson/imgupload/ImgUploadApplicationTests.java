f41b87a26972c004042e853baa0bcd11b1955aa6
Alisson Rego <alissondroid07@gmail.com>
2023-10-22 18:36:31 -0300

    Initial commit

---
+++ img-upload/src/test/java/edu/alisson/imgupload/ImgUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.alisson.imgupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
